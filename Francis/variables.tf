cea6bf70f266e84a393654e6085b83cad583b325
ajf013 <jeni13franc@gmail.com>
2026-04-16 22:08:37 +1000

    Initial commit

---
+++ Francis/variables.tf
@@ -0,0 +1,50 @@
+variable "location" {
+  description = "Azure region"
+  default     = "Australia East"
+}
+
+variable "resource_group_name" {
+  description = "Existing Resource Group name"
+  default     = "rg-terraform-demo"
+}
+
+variable "vnet_name" {
+  default = "vnet-demo"
+}
+
+variable "subnet_name" {
+  default = "subnet-demo"
+}
+
+variable "address_space" {
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  default = ["10.0.1.0/24"]
+}
+
+variable "public_ip_name" {
+  default = "pip-demo"
+}
+
+variable "nic_name" {
+  default = "nic-demo"
+}
+
+variable "vm_name" {
+  default = "vm-demo"
+}
+
+variable "vm_size" {
+  default = "Standard_B4s_v2"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  description = "VM password"
+  sensitive   = true
+}
---
+++ Raj/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-aue"
+    storage_account_name = "tfstateaue12345"
+    container_name       = "tfstate"
+    key                  = "Raj.tfstate"
+  }
+}
